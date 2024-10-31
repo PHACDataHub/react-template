9bbf6ab4fb8e8a76f47407f4288c12c5fae91fcf
samiatoui <atoui79@gmail.com>
2024-10-31 15:22:01 -0500

    button assigned active styling

--- reusable/src/Components/Navigation/Navigation.jsx
+++ reusable/src/Components/Navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../Navigation/Navbar.css'
 
 const Navigation = () => {
@@ -10,10 +10,10 @@ const Navigation = () => {
                     <h1>App Title</h1>
                     <ul>
                         <li>
-                            <Link to="/">Home</Link> {/* Link to Home (Sample page) */}
+                            <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>Home</NavLink> {/* Link to Home (Sample page) */}
                         </li>
                         <li>
-                            <Link to="/sample">Sample</Link> {/* Link to Sample route */}
+                            <NavLink to="/sample" className={({ isActive }) => (isActive ? 'active' : '')}>Sample</NavLink> {/* Link to Sample route */}
                         </li>
                         {/* Add more links as needed */}
                     </ul>
